045987916fd6a42636f7ce409b364cb51a0dd827
Dima <dima.dibb1@gmail.com>
2019-12-11 22:12:54 -0800

    finish department

--- routes/department-api-routes.js
+++ routes/department-api-routes.js
@@ -7,4 +7,31 @@ module.exports = function(app) {
       res.json(departments);
     });
   });
+
+  app.get("/api/department/:id", function(req, res) {
+    // 2; Add a join to include all of the Department's Product here
+    db.Department.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbDepartment) {
+      res.json(dbDepartment);
+    });
+  });
+
+  app.post("/api/department", function(req, res) {
+    db.Department.create(req.body).then(function(dbDepartment) {
+      res.json(dbDepartment);
+    });
+  });
+
+  app.delete("/api/department/:id", function(req, res) {
+    db.Department.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbDepartment) {
+      res.json(dbDepartment);
+    });
+  });
 };
